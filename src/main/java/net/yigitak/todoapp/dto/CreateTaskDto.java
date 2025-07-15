f5d1d4b13b972e58b55b3e1718f110fe3e61501d
Yigit Ak <ak.yigitmesut@gmail.com>
2025-07-15 16:05:24 +0300

    builder added for create task dto

--- src/main/java/net/yigitak/todoapp/dto/CreateTaskDto.java
+++ src/main/java/net/yigitak/todoapp/dto/CreateTaskDto.java
@@ -5,23 +5,31 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-
-@JsonInclude( JsonInclude.Include.NON_NULL )
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record CreateTaskDto(
 
-    @NotBlank( message = "Task title cannot be blank." ) String title ,
+        @NotBlank(message = "Task title cannot be blank.") String title,
 
-    @Valid Optional< List< CreateSubtaskDto > > subtasks ,
+        @Valid Optional<List<CreateSubtaskDto>> subtasks,
 
-    @FutureOrPresent( message = "Task cannot be assigned on a date in the past." ) Optional< LocalDate > dateAssigned ,
+        @FutureOrPresent(message = "Task cannot be assigned on a date in the past.") Optional<LocalDate> dateAssigned,
 
-    @FutureOrPresent( message = "Due date cannot be in the past." ) Optional< LocalDate > dateDue ,
+        @FutureOrPresent(message = "Due date cannot be in the past.") Optional<LocalDate> dateDue,
 
-    Optional< String > description
+        Optional<String> description
 
-) { }
+) {
+    public CreateTaskDto {
+        subtasks = subtasks == null ? Optional.empty() : subtasks;
+        dateAssigned = dateAssigned == null ? Optional.empty() : dateAssigned;
+        dateDue = dateDue == null ? Optional.empty() : dateDue;
+        description = description == null ? Optional.empty() : description;
+    }
+}
